ebe1b282bd8922070127d8cde95d46ff9f3d5ee7
Sushan Uchil <88882430+sushan4@users.noreply.github.com>
2023-01-29 17:33:15 +0530

    Update Cards.js
    removed / from img src

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,22 +3,22 @@ import Card from './Card'
 
 function Cards(){
     const [items, setItems] = useState([
-        { id: 1, img: '/img/html.png', stat: "" },
-        { id: 1, img: '/img/html.png', stat: "" },
-        { id: 2, img: '/img/css.png', stat: "" },
-        { id: 2, img: '/img/css.png', stat: "" },
-        { id: 3, img: '/img/js.png', stat: "" },
-        { id: 3, img: '/img/js.png', stat: "" },
-        { id: 4, img: '/img/scss.png', stat: "" },
-        { id: 4, img: '/img/scss.png', stat: "" },
-        { id: 5, img: '/img/react.png', stat: "" },
-        { id: 5, img: '/img/react.png', stat: "" },
-        { id: 6, img: '/img/vue.png', stat: "" },
-        { id: 6, img: '/img/vue.png', stat: "" },
-        { id: 7, img: '/img/angular.png', stat: "" },
-        { id: 7, img: '/img/angular.png', stat: "" },
-        { id: 8, img: '/img/nodejs.png', stat: "" },
-        { id: 8, img: '/img/nodejs.png', stat: "" }
+        { id: 1, img: 'img/html.png', stat: "" },
+        { id: 1, img: 'img/html.png', stat: "" },
+        { id: 2, img: 'img/css.png', stat: "" },
+        { id: 2, img: 'img/css.png', stat: "" },
+        { id: 3, img: 'img/js.png', stat: "" },
+        { id: 3, img: 'img/js.png', stat: "" },
+        { id: 4, img: 'img/scss.png', stat: "" },
+        { id: 4, img: 'img/scss.png', stat: "" },
+        { id: 5, img: 'img/react.png', stat: "" },
+        { id: 5, img: 'img/react.png', stat: "" },
+        { id: 6, img: 'img/vue.png', stat: "" },
+        { id: 6, img: 'img/vue.png', stat: "" },
+        { id: 7, img: 'img/angular.png', stat: "" },
+        { id: 7, img: 'img/angular.png', stat: "" },
+        { id: 8, img: 'img/nodejs.png', stat: "" },
+        { id: 8, img: 'img/nodejs.png', stat: "" }
     ].sort(() => Math.random() - 0.5))
 
     const [prev, setPrev] = useState(-1)
