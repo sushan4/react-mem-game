b7b1302b47546ea8eda477ab4bd62332e4f3d230
sushan4 <uchilsushan36@gmail.com>
2023-01-29 16:59:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Cards from "./components/Cards";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game ReactJS Pbl</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
